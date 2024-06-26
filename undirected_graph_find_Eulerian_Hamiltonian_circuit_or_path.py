aa1f1ae540ae53240c31412539afde9427a39b30
Duc Trung <trung.lyduc18@gmail.com>
2024-06-26 23:31:17 +0700

    COMPLETE: find Eulerian and Hamiltonian cycle / path for UndirectedGraph

--- undirected_graph_find_Eulerian_Hamiltonian_circuit_or_path.py
+++ undirected_graph_find_Eulerian_Hamiltonian_circuit_or_path.py
@@ -6,15 +6,22 @@
 from collections import defaultdict
 
 
-class Graph(object):
+class UndirectedGraph(object):
 
     def __init__(self, vertices: int):
         self.V = vertices  # No. of vertices
         self.numberOfEdges = 0
         self.graph = defaultdict(list)  # default dictionary to store graph
 
-    # function to add an edge to graph
     def addEdge(self, u: int, v: int):
+        """
+        Function to add an edge to graph.
+
+        Args:
+            u (int): starting vertex
+            v (int): finishing vertex
+        """
+
         self.graph[u].append(v)
         self.graph[v].append(u)
         self.numberOfEdges += 1
@@ -33,7 +40,7 @@ class Graph(object):
             return False
 
         # Start travel from a vertex with non-zero degree
-        self.travelToAllVertices(i, visited)
+        self.DFS(i, visited)
 
         # Check if all non-zero degree vertices are visited
         for i in range(self.V):
@@ -43,24 +50,38 @@ class Graph(object):
         return True
 
     # Travel to all non-zero degree vertices
-    def travelToAllVertices(self, vertex: int, visited: list):
+    def DFS(self, vertex: int, visited: list):
+        """
+        Depth First Search
+
+        Args:
+            vertex (int): current vertex
+            visited (list): visited list
+        """
         # Mark the current node as visited
         visited[vertex] = True
 
         # Recur for all the vertices adjacent to this vertex
         for i in self.graph[vertex]:
             if visited[i] == False:
-                self.travelToAllVertices(i, visited)
-
-    """The function returns one of the following values
-    0 --> If graph is not Eulerian
-    1 --> If graph has an Euler path (Semi-Eulerian)
-    2 --> If graph has an Euler Circuit (Eulerian) """
+                self.DFS(i, visited)
 
     def isEulerian(self):
-        # 1. Check if all non-zero degree vertices are connected
+        """The function returns one of the following values
+
+        0 --> If graph is not Eulerian
+
+        1 --> If graph has an Euler path (Semi-Eulerian)
+
+        2 --> If graph has an Euler circuit (Eulerian)
+
+        Note that odd count can never be 1 for undirected graph.
+
+        """
+
+        # Check if all non-zero degree vertices are connected
         # We don’t care about vertices with zero degree because
-        # they don’t belong to Eulerian Cycle or Path (we only consider all edges).
+        # they don’t belong to Eulerian Circuit or Path (we only consider all edges).
 
         if self.isConnected() == False:
             return 0
@@ -70,13 +91,6 @@ class Graph(object):
             for i in range(self.V):
                 if len(self.graph[i]) % 2 != 0:
                     odd += 1
-
-            """
-            If odd count is 0, then Eulerian
-            If odd count is 2, then semi-Eulerian.
-            If count is more than 2, then graph is not Eulerian
-            Note that odd count can never be 1 for undirected graph
-            """
 
             if odd == 0:
                 return 2
@@ -110,8 +124,26 @@ class Graph(object):
 
         return path
 
-    def findEulerianCycle(self):
-        # Find the Eulerian cycle
+    def findEulerianCircuit(self):
+        """
+        Find Eulerian circuit of the graph.
+
+        Eulerian circuit: a path that goes through each and every edge of the graph exactly once
+        and finishes at the starting vertex.
+
+        Eulerian path: a path that goes through each and every edge of the graph exactly once.
+
+        Returns:
+            tuple: (index, path)
+
+            index = 2 => Eulerian circuit
+
+            index = 1 => Eulerian path
+
+            index = 0 => no Eulerian circuit / path
+        """
+
+        # Find the Eulerian circuit
         eulerianType = self.isEulerian()
 
         if eulerianType == 2:
@@ -119,7 +151,7 @@ class Graph(object):
             while len(self.graph[i]) == 0:
                 i += 1
 
-            return self.travelThroughEdges(i, [], [i])
+            return (2, self.travelThroughEdges(i, [], [i]))
 
         # Find the Eulerian path
         elif eulerianType == 1:
@@ -127,51 +159,97 @@ class Graph(object):
             while len(self.graph[i]) % 2 != 1:
                 i += 1
 
-            return self.travelThroughEdges(i, i, [], [i])
+            return (1, self.travelThroughEdges(i, [], [i]))
 
-        # Neither the Eulerian cycle nor the Eulerian path
+        # Neither the Eulerian circuit nor the Eulerian path
         else:
-            return []
+            return (0, [])
 
-    def travelThroughVertices(self, currentVertex: int, path: list):
+    def travelThroughVertices(self, startVertex: int, currentVertex: int, path: list):
         for nextVertex in self.graph[currentVertex]:
-            
-            # Hamiltonian cycle / path
-            if len(path) == self.V + 1 and path[0] == path[-1] or len(path) == self.V:
+
+            # Hamiltonian circuit / path
+            if len(path) == self.V:
+                if startVertex in self.graph[currentVertex]:
+                    path.append(startVertex)
+
                 return path
-            
+
             if nextVertex not in path:
                 path.append(nextVertex)
-                path = self.travelThroughVertices(nextVertex, path)
-                
-                # Return immediately if it is Hamiltonian cycle / path
-                if len(path) == self.V + 1 and path[0] == path[-1] or len(path) == self.V:
+                path = self.travelThroughVertices(startVertex, nextVertex, path)
+
+                # Return immediately if it is Hamiltonian circuit / path
+                if (
+                    len(path) == self.V + 1
+                    and path[0] == path[-1]
+                    or len(path) == self.V
+                ):
                     return path
                 else:
                     path.pop()
 
             else:
                 continue
-                    
+
         return path
-    
-    def findHamiltonianCycle(self):
-        i = 0
-        while len(self.graph[i]) == 0:
-            i += 1
-            
-        self.travelThroughVertices(i, [i])
+
+    def findHamiltonianCircuit(self):
+        """
+        Find Hamiltonian circuit of the graph.
+
+        Hamiltonian circuit: a path that visits each and every vertex of the graph exactly once
+        and finishes at the starting vertex.
+
+        Hamiltonian path: a path that visits each and every vertex of the graph exactly once.
+
+        Returns:
+            tuple: (index, path)
+
+            index = 2 => Hamiltonian circuit
+
+            index = 1 => Hamiltonian path
+
+            index = 0 => no Hamiltonian circuit / Path
+        """
+
+        path = []
+        for vertex in range(self.V):
+            if len(self.graph[vertex]) > 0:
+                path = self.travelThroughVertices(vertex, vertex, [vertex])
+
+                # Hamiltonian circuit
+                if len(path) == self.V + 1 and path[0] == path[-1]:
+                    return (2, path)
+
+                # Hamiltonian path
+                elif len(path) == self.V:
+                    return (1, path)
+
+        # Neither the Hamiltonian circuit nor the Hamiltonian path
+        return (0, [])
+
 
 def main():
-    # g1 = Graph(5)
-    # g1.addEdge(1, 2)
-    # g1.addEdge(2, 0)
-    # g1.addEdge(0, 3)
-    # g1.addEdge(3, 4)
+    g1 = UndirectedGraph(6)
+    g1.addEdge(0, 1)
+    g1.addEdge(0, 2)
+    g1.addEdge(0, 3)
+    g1.addEdge(0, 5)
+    g1.addEdge(1, 2)
+    g1.addEdge(1, 4)
+    g1.addEdge(2, 3)
+    g1.addEdge(2, 4)
+    g1.addEdge(3, 4)
+    g1.addEdge(3, 5)
+    g1.addEdge(4, 5)
     # print(g1.isEulerian())
-    # print(g1.findEulerianCycle())
+    print("Find Eulerian circuit")
+    print(g1.findEulerianCircuit())
+    print("Find Hamiltonian circuit")
+    print(g1.findHamiltonianCircuit())
 
-    # g2 = Graph(11)
+    # g2 = UndirectedGraph(11)
     # g2.addEdge(2, 1)  # 1
     # g2.addEdge(1, 0)  # 2
     # g2.addEdge(0, 10)  # 3
@@ -191,7 +269,7 @@ def main():
     # g2.addEdge(7, 5)  # 17
     # g2.addEdge(10, 8)  # 18
     # print(g2.isEulerian())
-    # print(g2.findEulerianCycle())
+    # print(g2.findEulerianCircuit())
 
 
 if __name__ == "__main__":
