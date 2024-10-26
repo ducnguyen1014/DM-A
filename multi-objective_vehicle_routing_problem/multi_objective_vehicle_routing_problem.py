37a13f4c3110cec17030e6dc2aafbf703654ca39
Duc Trung <trung.lyduc18@gmail.com>
2024-10-27 01:46:32 +0700

    CHECKPOINT: complete define coordinates (restructure)

--- multi-objective_vehicle_routing_problem/multi_objective_vehicle_routing_problem.py
+++ multi-objective_vehicle_routing_problem/multi_objective_vehicle_routing_problem.py
@@ -1,5 +1,5 @@
 from pymoo.core.problem import ElementwiseProblem
-
+from typing import List, Tuple
 import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ import random
 
 # PARAMETERS ======================================================
 
-# Marker style mappings:
+# Coordinates style mappings:
 # '.': 'point'            # Small point marker
 # ',': 'pixel'            # Pixel marker
 # 'o': 'circle'           # Circle marker
@@ -36,41 +36,52 @@ import random
 # 'X': 'x_filled'         # Filled X marker
 
 
+# Roads style mappings:
+# Solid line:     '-'
+# Dashed line:    '--'
+# Dotted line:    ':'
+# Dash-dot line:  '-.'
+
 # Random
 random.seed(50)
 
 
 # Plot settings
-ENABLE_POINT_LABEL = False
+ENABLE_NDP_CUSTOMERS = True
+ENABLE_HDP_CUSTOMERS = True
+ENABLE_POINT_LABEL = True
 FIG_SIZE = (16, 8)
 
 # Depot - This problem contains only one depot
+DEPOT_NAME = "Depot"
 DEPOT_LOCATION = (100, 50)
 DEFAULT_COLOR_OF_DEPOT = "red"
-DEFAULT_marker_OF_DEPOT = "s"
-DEFAULT_marker_SIZE_OF_DEPOT = 100
+DEFAULT_MARKER_OF_DEPOT = "s"
+DEFAULT_MARKER_SIZE_OF_DEPOT = 100
 
 
 # NDP customers
+NDP_CUSTOMER_NAME = "NDP_Customer"
 NUMBER_OF_NDP_CUSTOMER = 5
 RANGE_OF_NDP_CUSTOMER = (
     (0, 200),
     (0, 100),
 )  # ((xlim), (ylim)), for example: ((0, 200), (0, 100))
 DEFAULT_COLOR_OF_NDP_CUSTOMER = "blue"
-DEFAULT_marker_OF_NDP_CUSTOMER = "o"
-DEFAULT_marker_SIZE_OF_NDP_CUSTOMER = 100
+DEFAULT_MARKER_OF_NDP_CUSTOMER = "o"
+DEFAULT_MARKER_SIZE_OF_NDP_CUSTOMER = 100
 
 
 # HDP customers
+HDP_CUSTOMER_NAME = "HDP_Customer"
 NUMBER_OF_HDP_CUSTOMER = 5
 RANGE_OF_HDP_CUSTOMER = (
     (0, 200),
     (0, 100),
 )  # ((xlim), (ylim)), for example: ((0, 200), (0, 100))
 DEFAULT_COLOR_OF_HDP_CUSTOMER = "green"
-DEFAULT_marker_OF_HDP_CUSTOMER = "^"
-DEFAULT_marker_SIZE_OF_HDP_CUSTOMER = 100
+DEFAULT_MARKER_OF_HDP_CUSTOMER = "^"
+DEFAULT_MARKER_SIZE_OF_HDP_CUSTOMER = 100
 
 
 # Map
@@ -78,7 +89,9 @@ RANGE_OF_MAP = (
     (0, 200),
     (0, 100),
 )  # 2D map, ((xlim), (ylim)), for example: ((0, 100), (0, 100))
-
+DEFAULT_COLOR_OF_ROAD = "black"
+DEFAULT_WIDTH_OF_ROAD = 2
+DEFAULT_STYLE_OF_ROAD = ""
 
 # Transportation
 NUMBER_OF_VEHICLE = 1
@@ -88,18 +101,24 @@ NUMBER_OF_VEHICLE = 1
 
 
 class Coordinates:
-    def __init__(self, latitude: float, longitude: float):
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        name: str = None,
+        marker: str = None,
+        marker_size: int = 100,
+        marker_color: str = None,
+    ):
         self.latitude = float(latitude)
         self.longitude = float(longitude)
+        self.name = name
+        self.marker = marker
+        self.marker_size = marker_size
+        self.marker_color = marker_color
 
     def __repr__(self):
         return f"Coordinates(latitude={self.latitude}, longitude={self.longitude})"
-
-    def flat_distance_to(self, other):
-        coord1 = np.array([self.latitude, self.longitude])
-        coord2 = np.array([other.latitude, other.longitude])
-
-        return np.linalg.norm(coord1 - coord2)
 
 
 class Depot(Coordinates):
@@ -107,17 +126,19 @@ class Depot(Coordinates):
         self,
         latitude: float,
         longitude: float,
-        name: str = "Depot",
-        marker: str = DEFAULT_marker_OF_DEPOT,
-        marker_size: int = DEFAULT_marker_SIZE_OF_DEPOT,
+        name: str = DEPOT_NAME,
+        marker: str = DEFAULT_MARKER_OF_DEPOT,
+        marker_size: int = DEFAULT_MARKER_SIZE_OF_DEPOT,
         marker_color: str = DEFAULT_COLOR_OF_DEPOT,
     ):
-        super().__init__(latitude=latitude, longitude=longitude)
-
-        self.name = name
-        self.marker = marker
-        self.marker_size = marker_size
-        self.marker_color = marker_color
+        super().__init__(
+            latitude=latitude,
+            longitude=longitude,
+            name=name,
+            marker=marker,
+            marker_size=marker_size,
+            marker_color=marker_color,
+        )
 
 
 class Customer(Coordinates):
@@ -130,98 +151,185 @@ class Customer(Coordinates):
         marker_size: int,
         marker_color: str,
     ):
-        super().__init__(latitude=latitude, longitude=longitude)
-        self.name = name
-        self.marker = marker
-        self.marker_size = marker_size
-        self.marker_color = marker_color
-
-
-class NDP_Customer(Customer):
-    def __init__(self, latitude: float, longitude: float):
         super().__init__(
             latitude=latitude,
             longitude=longitude,
-            name="NDP_Customer",
-            marker=DEFAULT_marker_OF_NDP_CUSTOMER,
-            marker_size=DEFAULT_marker_SIZE_OF_NDP_CUSTOMER,
-            marker_color=DEFAULT_COLOR_OF_NDP_CUSTOMER,
+            name=name,
+            marker=marker,
+            marker_size=marker_size,
+            marker_color=marker_color,
+        )
+
+
+class NDP_Customer(Customer):
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        name: str = NDP_CUSTOMER_NAME,
+        marker: str = DEFAULT_MARKER_OF_NDP_CUSTOMER,
+        marker_size: int = DEFAULT_MARKER_SIZE_OF_NDP_CUSTOMER,
+        marker_color: str = DEFAULT_COLOR_OF_NDP_CUSTOMER,
+    ):
+        super().__init__(
+            latitude=latitude,
+            longitude=longitude,
+            name=name,
+            marker=marker,
+            marker_size=marker_size,
+            marker_color=marker_color,
         )
 
 
 class HDP_Customer(Customer):
-    def __init__(self, latitude: float, longitude: float):
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        name: str = HDP_CUSTOMER_NAME,
+        marker: str = DEFAULT_MARKER_OF_HDP_CUSTOMER,
+        marker_size: int = DEFAULT_MARKER_SIZE_OF_HDP_CUSTOMER,
+        marker_color: str = DEFAULT_COLOR_OF_HDP_CUSTOMER,
+    ):
         super().__init__(
             latitude=latitude,
             longitude=longitude,
-            name="HDP_Customer",
-            marker=DEFAULT_marker_OF_HDP_CUSTOMER,
-            marker_size=DEFAULT_marker_SIZE_OF_HDP_CUSTOMER,
-            marker_color=DEFAULT_COLOR_OF_HDP_CUSTOMER,
+            name=name,
+            marker=marker,
+            marker_size=marker_size,
+            marker_color=marker_color,
         )
+
+
+class Road:
+    def __init__(self, start: Coordinates, end: Coordinates):
+        self.start = start
+        self.end = end
+        self.distance = self.calculate_distance()
+
+    def calculate_distance(self):
+        coord1 = np.array([self.start.latitude, self.start.longitude])
+        coord2 = np.array([self.start.latitude, self.start.longitude])
+
+        return np.linalg.norm(coord1 - coord2)
+
+    def get_distance(self):
+        return self.distance
 
 
 class MapGraph:
     def __init__(self):
-        # Dictionary to store graph adjacency list
-        # The keys are Coordinates objects, and the values are lists of tuples (neighbor, distance)
+        self.coordinate_list: List[Coordinates] = []
+        self.road_list: List[Tuple[Coordinates, Coordinates, float]] = []
         self.graph = defaultdict(list)
-        self.depot: Depot = None
-        self.customer_list: list[Customer] = []
 
-    def set_depot(self, new_depot: Depot):
-        """
-        Set a new depot in the graph, removing any existing depot if present.
-        """
-        # Remove the old depot if it exists
-        if hasattr(self, "depot") and self.depot in self.graph:
-            del self.graph[self.depot]
-
-        # Set the new depot
-        self.depot = new_depot
-
-        # Add the new depot to the graph if it’s not already there
-        if new_depot not in self.graph:
-            self.graph[new_depot] = []  # Initialize adjacency list for the new location
-
-    def add_customer(self, customer: Customer):
-        """
-        Add a new customer to the graph.
-        """
-        if customer not in self.graph:
-            self.graph[customer] = []  # Initialize adjacency list for new location
+    def add_location(self, location: Coordinates):
+        if location not in self.coordinate_list:
+            self.coordinate_list.append(location)
+            self.graph[location] = []
 
     def road_exists(self, start: Coordinates, end: Coordinates):
-        """
-        Check if a road (edge) already exists between two coordinates.
-        """
-        # Check if 'end' is already a neighbor of 'start'
         for neighbor, _ in self.graph[start]:
             if neighbor == end:
                 return True
         return False
 
     def add_road(self, start: Coordinates, end: Coordinates):
-        """
-        Add a road (edge) with distance between two coordinates to the graph.
-        Avoid adding duplicate roads.
-        """
+        # Check wether the coordinates exist in the graph
         if start not in self.graph:
             self.add_location(start)
         if end not in self.graph:
             self.add_location(end)
 
-        # Check if the road already exists to avoid duplicates
         if not self.road_exists(start, end) and not self.road_exists(end, start):
-            # Calculate the distance between the two points
             distance = start.flat_distance_to(end)
-
-            # Add the road (edge) in both directions (undirected graph)
             self.graph[start].append((end, distance))
             self.graph[end].append((start, distance))
-
+            # Add the road to the roads list
+            self.roads.append((start, end, distance))
         else:
             raise ValueError(f"Road between {start} and {end} already exists")
+
+    def remove_road(self, start: Coordinates, end: Coordinates):
+        """
+        Remove the road (edge) between two coordinates if it exists.
+        """
+        # Remove end from start's neighbors
+        self.graph[start] = [
+            (neighbor, dist) for neighbor, dist in self.graph[start] if neighbor != end
+        ]
+
+        # Remove start from end's neighbors
+        self.graph[end] = [
+            (neighbor, dist) for neighbor, dist in self.graph[end] if neighbor != start
+        ]
+
+        # Remove the road from the roads list
+        self.roads = [
+            (s, e, d)
+            for s, e, d in self.roads
+            if not ((s == start and e == end) or (s == end and e == start))
+        ]
+
+    def add_all_roads(self):
+        """
+        Adds roads between all pairs of points in the graph if a road doesn't already exist.
+        """
+        locations = list(self.graph.keys())
+
+        for i in range(len(locations)):
+            for j in range(i + 1, len(locations)):
+                start, end = locations[i], locations[j]
+
+                # Add a road if one does not already exist
+                if not self.road_exists(start, end):
+                    self.add_road(start, end)
+
+    def compose_visualization_coordinates(self):
+        """
+        Visualize the graph with coordinates as points.
+        """
+
+        unique_label: list[str] = []
+
+        for coordinate in self.coordinate_list:
+            # Plot all coordinates
+
+            if not ENABLE_NDP_CUSTOMERS and coordinate.name == NDP_CUSTOMER_NAME:
+                continue
+
+            if not ENABLE_HDP_CUSTOMERS and coordinate.name == HDP_CUSTOMER_NAME:
+                continue
+
+            plt.scatter(
+                coordinate.latitude,
+                coordinate.longitude,
+                color=coordinate.marker_color,
+                marker=coordinate.marker,
+                s=coordinate.marker_size,
+                label=coordinate.name if coordinate.name not in unique_label else "",
+            )
+
+            if ENABLE_POINT_LABEL:
+                plt.text(
+                    coordinate.latitude + 1,
+                    coordinate.longitude + 1,
+                    coordinate.name,
+                    fontsize=9,
+                )
+
+            unique_label.append(coordinate.name)
+
+    def visualize(self):
+        plt.xlabel("Latitude")
+        plt.ylabel("Longitude")
+        plt.title("Map Graph Visualization")
+        plt.legend(loc="upper left")
+        plt.xlim(RANGE_OF_MAP[0])
+        plt.ylim(RANGE_OF_MAP[1])
+        plt.grid(True)
+        # plt.tight_layout()
+        plt.show()
 
 
 class MultiObjectiveVehicleRoutingProblem(ElementwiseProblem):
@@ -270,97 +378,47 @@ class MultiObjectiveVehicleRoutingProblem(ElementwiseProblem):
 
         self.map_graph = MapGraph()
 
-        self.map_graph.set_depot(self.depot)
+        # Add depot
+        self.map_graph.add_location(self.depot)
 
         for customer in self.ndp_customer_list:
-            self.map_graph.add_customer(customer)
+            self.map_graph.add_location(customer)
 
         for customer in self.hdp_customer_list:
-            self.map_graph.add_customer(customer)
+            self.map_graph.add_location(customer)
 
-    def visualize_coordinates(self):
+    def compose_roads(self):
         """
-        Visualize the graph with coordinates as points.
+        Visualize the roads on a 2D plot using matplotlib.
         """
+        plt.figure(figsize=(10, 8))
+
+        # Plot each road
+        for start, end, distance in self.map_graph.roads:
+            plt.plot(
+                [start, end],
+                [start.y, end.y],
+                "b-",  # Blue color, circle markers, solid line
+                markersize=5,
+            )
+
+    def visualize(self, enable_coordinates: bool = True, enable_roads: bool = True):
         plt.figure(figsize=FIG_SIZE)
 
-        # Flag to set labels only once
-        depot_label_set = False
-        ndp_customer_label_set = False
-        hdp_customer_label_set = False
+        if not enable_coordinates and not enable_roads:
+            raise Warning("Please enable at least one option to visualize")
+        if enable_coordinates:
+            self.map_graph.compose_visualization_coordinates()
+        # if enable_roads:
+        #     self.compose_roads()
 
-        # Plot depot
-        plt.scatter(
-            self.depot.latitude,
-            self.depot.longitude,
-            label=self.depot.name if not depot_label_set else "",
-            color=self.depot.marker_color,
-            marker=self.depot.marker,
-            s=self.depot.marker_size,
-        )
-        if ENABLE_POINT_LABEL:
-            plt.text(
-                self.depot.latitude + 1,
-                self.depot.longitude + 1,
-                self.depot.name,
-                fontsize=9,
-            )
-        depot_label_set = True  # Set flag after plotting depot
-
-        # Plot NDP customers
-        for customer in self.ndp_customer_list:
-            # Plot the coordinates
-            plt.scatter(
-                customer.latitude,
-                customer.longitude,
-                color=customer.marker_color,
-                s=customer.marker_size,
-                label=customer.name if not ndp_customer_label_set else "",
-            )
-            if ENABLE_POINT_LABEL:
-                plt.text(
-                    customer.latitude + 1,
-                    customer.longitude + 1,
-                    customer.name,
-                    fontsize=9,
-                )
-            ndp_customer_label_set = True  # Set flag after plotting first customer
-
-        # Plot HDP customers
-        for customer in self.hdp_customer_list:
-            # Plot the coordinates
-            plt.scatter(
-                customer.latitude,
-                customer.longitude,
-                color=customer.marker_color,
-                s=customer.marker_size,
-                label=customer.name if not hdp_customer_label_set else "",
-            )
-            if ENABLE_POINT_LABEL:
-                plt.text(
-                    customer.latitude + 1,
-                    customer.longitude + 1,
-                    customer.name,
-                    fontsize=9,
-                )
-            hdp_customer_label_set = True  # Set flag after plotting first customer
-
-        # Adding labels and showing the plot
-        plt.xlabel("Longitude")
-        plt.ylabel("Latitude")
-        plt.title("Map Graph Visualization")
-        plt.legend(loc="upper left")
-        plt.xlim(RANGE_OF_MAP[0])
-        plt.ylim(RANGE_OF_MAP[1])
-        plt.grid(True)
-        # plt.tight_layout()
-        plt.show()
+        self.map_graph.visualize()
 
 
 def main():
     problem = MultiObjectiveVehicleRoutingProblem()
 
-    problem.visualize_coordinates()
+    problem.visualize()
 
 
 if __name__ == "__main__":
